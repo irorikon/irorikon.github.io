334e117ef9c3ee421fd681816b4f14df8513de92
irorikon <irorikon@88.com>
2022-04-01 06:07:49 -0700

    init

--- js/main.js
+++ js/main.js
@@ -79,24 +79,4 @@ $(document).ready(function () {
      * 底部年份动态化
      */
     $('.year').html(new Date().getFullYear());
-
-    /**
-     * 异步加载一言
-     */
-    (function getHitokoto() {
-        $.ajax({
-            //url: "https://api.imjad.cn/hitokoto/?encode=jsc&charset=utf-8&length=50",
-            url: "https://v1.hitokoto.cn/?encode=json&charset=utf-8",
-            dataType: "jsonp",
-            async: true,
-            jsonp: "callback",
-            jsonpCallback: "hitokoto",
-            success: function (result) {
-                $('#hitokoto').html("<p>" + result.hitokoto + "</p>")
-            },
-            error: function () {
-                $('#hitokoto').html("<p>读取数据失败了的说……_(:з」∠)_</p>")
-            }
-        });
-    })();
 });
